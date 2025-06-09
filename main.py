8a88bf53519a6d8a54da4e504da8237e86a16ca6
krrishjain <krrishjain2308@gmail.com>
2025-06-09 20:10:17 +0530

    add lines in pages

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times",style="B",size=20)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0,h=12,txt=row["Topic"],align="L",ln=1)
+    for y in range(20, 298, 10):
+        pdf.line(8,y, 202, y)
     pdf.line(8,20, 202, 20)
 
     pdf.ln(266)
@@ -19,6 +21,9 @@ for index, row in df.iterrows():
     for i in range(row["Pages"] - 1):
         pdf.add_page()
 
+        for y in range(20, 298, 10):
+            pdf.line(8, y, 202, y)
+        pdf.line(8, 20, 202, 20)
         pdf.ln(278)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
